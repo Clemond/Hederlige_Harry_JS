02dbedca762b09809d004df10eef522db7ec20db
Nicholas Nieminen Jönsson <nicholasnieminenjonsson@MacBook-Pro-som-tillhor-Nicholas.local>
2024-01-05 12:07:50 +0100

    Third Assignment

--- script.js
+++ script.js
@@ -5,19 +5,25 @@
 // (finns inget ID idag) som kör en funktion när man klickar på knappen.
 // Funktionen som körs när man klickar på knappen ska läsa texten från
 // text-input:en med id "email" och logga den texten i konsolen med console.log
-// Glöm ej pusha till git
+// Glöm ej pusha till github
 
 const contactButton = document.getElementById("contact-button");
 
 contactButton.addEventListener("click", () => {
-  const emailInput = document.getElementById("email_input").value;
+  let emailInput = document.getElementById("email_input").value;
   console.log(emailInput);
 });
 
 // 3. Skriv en funktion createParagraph som tar emot en text som parameter.
 // Funktionen ska sedan skapa en <p>-tag med den inskickade texten som värde.
 // Funktionen ska sedan returnera den skapade <p>-taggen.
-// Glöm ej pusha till git
+// Glöm ej pusha till github
+
+function createParagraph(text) {
+  let newParagraph = document.createElement("p");
+  newParagraph.textContent = text;
+  return text;
+}
 
 // 4. Varje gång man ÄNDRAR på phone så ska funktionen createParagraph
 // anropas - du skickar med värdet i phone och p-taggen som returneras ska
