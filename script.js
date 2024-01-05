8fc25e7002f23b6c2033f9fcd938f8ed7441d96f
Nicholas Nieminen Jönsson <nicholasnieminenjonsson@MacBook-Pro-som-tillhor-Nicholas.local>
2024-01-05 10:35:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Krav för G:
+// 1. skapa en script.js-fil som anropas från HTML-filen
+
+// 2. Lägg till en eventlyssnare på knappen med t.ex id "button-contact"
+// (finns inget ID idag) som kör en funktion när man klickar på knappen.
+// Funktionen som körs när man klickar på knappen ska läsa texten från
+// text-input:en med id "email" och logga den texten i konsolen med console.log
+// Glöm ej pusha till git
+
+// 3. Skriv en funktion createParagraph som tar emot en text som parameter.
+// Funktionen ska sedan skapa en <p>-tag med den inskickade texten som värde.
+// Funktionen ska sedan returnera den skapade <p>-taggen.
+// Glöm ej pusha till git
+
+// 4. Varje gång man ÄNDRAR på phone så ska funktionen createParagraph
+// anropas - du skickar med värdet i phone och p-taggen som returneras ska
+// skrivas till console.log
+// Glöm ej pusha till git
